20358677429633d84b67239d9e6eeeede29033f6
Matt White <mwhite1@2u.com>
2022-02-01 12:38:25 -0500

    remove one log

--- src/Components/OneMore.js
+++ src/Components/OneMore.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class OneMore extends Component {
-    componentDidMount() {
-        console.log("hello there")
-    }
-    
     render() {
         return ( 
             <div>One more class based Component.</div>
