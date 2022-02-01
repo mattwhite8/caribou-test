14a5a8da5d0541696c99da5be34123eafd0f5b53
Matt White <mwhite1@2u.com>
2022-02-01 12:34:05 -0500

    adding logs for fun

--- src/Components/Another.js
+++ src/Components/Another.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Another extends Component {
+    componentDidMount() {
+        console.log("hello again")
+    }
+
     render() {
         return ( 
             <div>Here is another old school component.</div>
--- src/Components/OneMore.js
+++ src/Components/OneMore.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class OneMore extends Component {
+    componentDidMount() {
+        console.log("hello there")
+    }
+    
     render() {
         return ( 
             <div>One more class based Component.</div>
